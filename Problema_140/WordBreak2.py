b244b40d30e07b0d324e181e2e193cc4cab53784
Pedro Augusto Dourado Izarias <pedroaugustoizarias@gmail.com>
2025-02-10 22:51:37 -0300

    Update WordBreak2.py

--- Problema_140/WordBreak2.py
+++ Problema_140/WordBreak2.py
@@ -1,13 +1,21 @@
 def wordBreak(s, wordDict):
-    wordSet = set(wordDict)  
+    wordSet = set(wordDict)
     result = []
     
+    def backtrack(start, current):
+        
+        if start == len(s):
+            result.append(" ".join(current))
+            return
+        
+        
+        for end in range(start + 1, len(s) + 1):
+            if s[start:end] in wordSet:
+                backtrack(end, current + [s[start:end]])  
 
-    for i in range(1, len(s) + 1):
-        if s[:i] in wordSet: 
-            result.append(s[:i]) 
-            
+    backtrack(0, [])
     return result
+
 
 s = "catsanddog"
 wordDict = ["cat", "cats", "and", "sand", "dog"]
