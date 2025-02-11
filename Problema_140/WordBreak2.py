6aea2755757cf09de952c921c5e2449a22df4cdc
Pedro Augusto Dourado Izarias <pedroaugustoizarias@gmail.com>
2025-02-10 22:55:54 -0300

    Update WordBreak2.py

--- Problema_140/WordBreak2.py
+++ Problema_140/WordBreak2.py
@@ -1,19 +1,18 @@
-def wordBreak(s, wordDict):
-    wordSet = set(wordDict)
-    n = len(s)
-    dp = [[] for _ in range(n + 1)]  
-    dp[0] = ['']  
-    
-   
-    for i in range(1, n + 1):
-        for j in range(i):
-            if s[j:i] in wordSet:
-                for sentence in dp[j]:
-                    dp[i].append(sentence + ('' if sentence == '' else ' ') + s[j:i])
-    
-    return dp[n]
-
+class Solution:
+    def wordBreak(self, s, wordDict):
+        wordSet = set(wordDict)
+        n = len(s)
+        dp = [[] for _ in range(n + 1)]  
+        dp[0] = ['']  
+        for i in range(1, n + 1):
+            for j in range(i):
+                if s[j:i] in wordSet:
+                    for sentence in dp[j]:
+                        dp[i].append(sentence + ('' if sentence == '' else ' ') + s[j:i])
+        
+        return dp[n]
 
 s = "catsanddog"
 wordDict = ["cat", "cats", "and", "sand", "dog"]
-print(wordBreak(s, wordDict))
+sol = Solution()
+print(sol.wordBreak(s, wordDict))
